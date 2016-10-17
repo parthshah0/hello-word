e195c5ddaf3384916ffb7a512ed7eb975bbfcd38
parthshah0 <parthshah9867@gmail.com>
2016-10-17 11:19:07 +0530

    Add files via upload

---
+++ swap2.py
@@ -0,0 +1,15 @@
+a=int(input("enter first number\n"))
+b=int(input("enter second number\n"))
+c=int(input("enter third number\n"))
+if a>b:
+    if a>c:
+        print("%d is greater\n"%a)
+    else:
+        print("%d is greater\n"%c)
+else:
+    if b>c:
+        print("%d is gerater\n"%b)
+    else:
+        print("%d is greater\n"%c)
+
+
